c6759b4a5b975737c2e8e759e58ea83c119a7039
Anders Forsell <aforsell1971@gmail.com>
2014-07-06 00:52:22 +0200

    Change the example to a polymer element with bindings to chess-board

---
+++ example/chess_board_app.dart
@@ -0,0 +1,15 @@
+/* Copyright (c) 2014, Anders Forsell (aforsell1971@gmail.com)
+ * Released under the MIT license
+ * https://github.com/andersforsell/chessboard.dart/blob/master/LICENSE
+ */
+
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer chessboard app for testing.
+ */
+@CustomTag('chess-board-app')
+class ChessBoardApp extends PolymerElement {
+
+  ChessBoardApp.created() : super.created();
+}
